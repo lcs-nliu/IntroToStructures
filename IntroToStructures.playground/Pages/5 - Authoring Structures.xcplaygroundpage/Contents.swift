bfe34280c51e38306de3bff904c71d182f98938e
Nicole Liu <nicoleliu_@outlook.com>
2020-02-21 13:57:10 -0500

    Finished a structure for 1 2D shape

--- IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
+++ IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
@@ -32,6 +32,61 @@ import Foundation
  
  */
 // Begin your work here...
+// Define a Parallelogram
+struct Parallelogram {
+    
+    // MARK: Properties
+    var base: Double
+    var height: Double
+    var side: Double
+
+    // Computed property to return a description of the shape
+    var shapeDescription: String {
+        return "A parallelogram is a flat shape with 4 straight sides where opposite sides are parallel."
+    }
+    
+    // Computed property to return the raw area
+    var area: Double {
+        return base * height
+    }
+    
+    
+    // Computed property to return the perimeter
+    var perimeter: Double {
+        return 2*(base+side)
+    }
+    
+    // Computed property to return a description of the area
+    var areaDescription: String {
+        return "The area of the parallelogram is " + String(format: "%.1f", area) + " square units."
+    }
+    
+    // Computed property to return a description of the perimeter
+    var perimeterDescription: String {
+        return "The perimeter of the parallelogram is " + String(format: "%.1f", perimeter) + " square units."
+        
+    
+    }
+    
+}
+
+// Create a parallelogram instance
+var someParallelogram = Parallelogram(base: 20, height: 5, side:5)
+
+// What is the description?
+print(someParallelogram.shapeDescription)
+
+// What is the area?
+someParallelogram.area
+
+// What is the perimeter?
+someParallelogram.perimeter
+
+// Report on the area?
+print(someParallelogram.areaDescription)
+
+// Report on the perimeter?
+print(someParallelogram.perimeterDescription)
 
 
 
